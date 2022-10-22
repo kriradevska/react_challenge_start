a061adb6359c24549173b0321d96779bd85c112e
Kristina Radevska <kriradevska@gmail.com>
2022-10-22 20:44:48 +0200

    appointment page state add, handleSubmit, appointment form and tile list add to render

--- react_challenge_start/src/containers/contactsPage/ContactsPage.js
+++ react_challenge_start/src/containers/contactsPage/ContactsPage.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = ({ contacts, addContact }) => {
+export const ContactsPage = ({ contacts, addContacts }) => {
   /*
   Define state variables for 
   contact info and duplicate check
@@ -22,7 +22,7 @@ export const ContactsPage = ({ contacts, addContact }) => {
     if the contact name is not a duplicate
     */
     if (!duplicate) {
-      addContact(name, email, phone);
+      addContacts(name, email, phone);
       setName('');
       setEmail('');
       setPhone('');
