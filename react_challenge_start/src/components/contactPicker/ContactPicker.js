4624b1bff1331fb819c8e757609961fb4790fdd8
Kristina Radevska <kriradevska@gmail.com>
2022-10-22 23:23:45 +0200

    contact selector add

--- react_challenge_start/src/components/contactPicker/ContactPicker.js
+++ react_challenge_start/src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({name, onChange, contacts}) => {
   return (
-    ContactPicker
+    <label>Choose contact:
+      <select onChange={onChange} name={name}>
+        <option value={''} key={-1} selected="selected">No Contact Selected</option>
+        {contacts.map((contact) => {
+          return <option value={contact} key={contact}>{contact}</option>
+        })}
+      </select>
+    </label>
   );
 };
